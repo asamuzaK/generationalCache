82ed12638119bccf87ff2b69025ed02e75f03fc4
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-04-12 09:25:23 +0900

    Update commander.js

--- scripts/commander.js
+++ scripts/commander.js
@@ -4,56 +4,8 @@
 
 /* api */
 import path from 'node:path';
-import process from 'node:process';
 import { program as commander } from 'commander';
-import { throwErr } from './common.js';
-import { createFile, isDir, readFile, removeDir } from './file-util.js';
-
-/* constants */
-const CHAR = 'utf8';
-const DIR_CWD = process.cwd();
-const INDENT = 2;
-
-/**
- * create deno.json
- * @param {boolean} info - console info
- * @returns {Promise.<string>} - file path
- */
-export const createDenoJson = async info => {
-  const pkg = await readFile(path.join(DIR_CWD, 'package.json'), {
-    encoding: CHAR,
-    flag: 'r'
-  });
-  const { dependencies } = JSON.parse(pkg);
-  const items = Object.entries(dependencies);
-  const importMap = new Map();
-  for (const [key, value] of items) {
-    const version = value.replace(/^[\^~]/, '');
-    importMap.set(key, `https://esm.sh/${key}@${version}`);
-  }
-  const obj = {
-    imports: Object.fromEntries(importMap)
-  };
-  const content = `${JSON.stringify(obj, null, INDENT)}\n`;
-  const filePath = await createFile(
-    path.resolve(DIR_CWD, 'deno.json'),
-    content
-  );
-  if (filePath && info) {
-    console.info(`Created: ${filePath}`);
-  }
-  return filePath;
-};
-
-/**
- * create deno config file
- * @param {object} cmdOpts - command options
- * @returns {Promise} - promise chain
- */
-export const createDenoConfigFile = (cmdOpts = {}) => {
-  const { info } = cmdOpts;
-  return createDenoJson(info).catch(throwErr);
-};
+import { isDir, removeDir } from './file-util.js';
 
 /**
  * clean directory
@@ -85,16 +37,7 @@ export const parseCommand = args => {
         .option('-d, --dir <name>', 'specify directory')
         .option('-i, --info', 'console info')
         .action(cleanDirectory);
-    } else if (args.includes('denoconf')) {
-      commander
-        .command('denoconf')
-        .description('create deno config file')
-        .option('-i, --info', 'console info')
-        .action(createDenoConfigFile);
     }
     commander.parse(args);
   }
 };
-
-/* For test */
-export { commander };
